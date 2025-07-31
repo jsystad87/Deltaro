ced3a1156b27baab4a658f5c7310b93b82f88f45
jsystad87 <82894658+jsystad87@users.noreply.github.com>
2025-07-31 10:34:46 +0200

    Create page.tsx
    legg til-opprett side

---
+++ pages/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import OpprettSkjema from "@/components/OpprettSkjema";
+
+export default function OpprettSide() {
+  return (
+    <main className="p-4 max-w-xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Opprett nytt arrangement</h1>
+      <OpprettSkjema />
+    </main>
+  );
