67ea15e7029618031cb9ed5a795af529b7f73668
jsystad87 <82894658+jsystad87@users.noreply.github.com>
2025-08-01 07:36:44 +0200

    Update page.tsx

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import OpprettSkjema from "@/Components/OpprettSkjema";
+
+import OpprettSkjema from "../Components/OpprettSkjema";
 
 export default function OpprettSide() {
   return (
-    <main className="p-4 max-w-xl mx-auto">
+    <main className="p-8 max-w-xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Opprett nytt arrangement</h1>
       <OpprettSkjema />
     </main>
   );
+}
