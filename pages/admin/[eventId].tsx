bf03387f25a2466596490b8cab4dc7bfddd548cd
jsystad87 <82894658+jsystad87@users.noreply.github.com>
2025-08-01 11:25:42 +0200

    Update [eventId].tsx

--- pages/admin/[eventId].tsx
+++ pages/admin/[eventId].tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { createClient } from '@/lib/supabase';
+import { supabase } from '@/lib/supabase';
 
 export default function AdminSubmissionsPage() {
   const router = useRouter();
